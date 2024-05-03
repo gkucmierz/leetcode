79b4ca0ba939b8a8904546d394c55c33ebb38772
Grzegorz Kućmierz <gkucmierz@gmail.com>
2024-05-03 17:38:42 +0200

    add: divisorGame

--- assessment.js
+++ assessment.js
@@ -17,3 +17,27 @@ const removeOuterParentheses = s => {
 removeOuterParentheses('(()())(())');
 removeOuterParentheses('(()())(())(()(()))')
 removeOuterParentheses('()()');
+
+
+
+
+//  2: A1
+//  3: A1 B1
+//  4: A1 B1 A1
+//  5: A1 B1 A1 B1
+//  6: A3 B1 A1
+//  7: A1 B3 A1 B1
+//  8: A1 B1 A3 B1 A1
+//  9: A3 B3 A1 B1
+// 10: A5 B1 A1 B1 A1
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+const divisorGame = n => {
+  return n % 2 === 0;
+};
+
+divisorGame(2);
+divisorGame(3);
