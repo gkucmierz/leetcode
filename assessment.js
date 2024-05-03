c1a230d9b9bd2ca1c092fbd27a71dde8ec152d0c
Grzegorz Kućmierz <gkucmierz@gmail.com>
2024-05-03 20:31:22 +0200

    add: nextClosestTime

--- assessment.js
+++ assessment.js
@@ -126,3 +126,49 @@ const kEmptySlots = (bulbs, k) => {
 kEmptySlots([1,3,2], 1);
 kEmptySlots([1,2,3], 1);
 kEmptySlots([3,9,2,8,1,6,10,5,4,7], 1);
+
+
+
+/**
+ * @param {string} time
+ * @return {string}
+ */
+const nextClosestTime = time => {
+  const MAX = 24 * 60;
+  const toScalar = str => {
+    const [h, s] = str.split(':').map(n => +n);
+    return h * 60 + s;
+  };
+  const target = toScalar(time);
+  const digits = [...new Set([...time.replace(':', '')])];
+  const size = digits.length;
+  const len = size ** 4;
+  let best = Infinity;
+  let res;
+  for (let i = 0 ; i < len; ++i) {
+    let n = i;
+    const candidate = [];
+    for (let j = 0; j < 4; ++j) {
+      const mod = n % size;
+      candidate.push(digits[mod]);
+      n = (n - mod) / size;
+    }
+    const h = candidate[0] + candidate[1];
+    const s = candidate[2] + candidate[3];
+    if (+h > 23) continue;
+    if (+s > 59) continue;
+    const timeStr = `${h}:${s}`;
+    const scalar = toScalar(timeStr);
+    let diff = scalar - target;
+    if (diff <= 0) diff += MAX;
+    if (diff < best) {
+      best = diff;
+      res = timeStr;
+    }
+  }
+  return res;
+};
+
+nextClosestTime('19:34');
+nextClosestTime('23:59');
+nextClosestTime('00:00');
