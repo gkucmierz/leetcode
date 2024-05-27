42dd03a5de22b605179594774ae40e5da52315dc
Grzegorz Kućmierz <gkucmierz@gmail.com>
2024-05-27 22:35:48 +0200

    add backspaceCompare

--- assessment.js
+++ assessment.js
@@ -1181,3 +1181,29 @@ const countNodes = root => {
   }
   return measure2(root, min + 1, lvl - 1) === lvl ? min + 1 : min;
 };
+
+
+
+
+
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const backspaceCompare = (s, t) => {
+  const clean = str => {
+    let last;
+    while (last !== str) {
+      last = str;
+      str = str.replace(/(\w#)|(^#+)/g, a => '');
+    }
+    return str
+  };
+  return clean(s) === clean(t);
+};
+
+backspaceCompare("ab#c", "ad#c");
+backspaceCompare("ab##", "c#d#");
+backspaceCompare("a#c", "b");
